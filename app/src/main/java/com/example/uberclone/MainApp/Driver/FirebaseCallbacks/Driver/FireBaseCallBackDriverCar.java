5d56d2f652c2cd59e60e5862844f673a187d06d8
Jusuf <jusuftopic@yahoo.com>
2021-05-27 07:20:54 +0200

    renamed parameter

--- app/src/main/java/com/example/uberclone/MainApp/Driver/FirebaseCallbacks/Driver/FireBaseCallBackDriverCar.java
+++ app/src/main/java/com/example/uberclone/MainApp/Driver/FirebaseCallbacks/Driver/FireBaseCallBackDriverCar.java
@@ -2,5 +2,5 @@ package com.example.uberclone.MainApp.Driver.FirebaseCallbacks.Driver;
 
 public interface FireBaseCallBackDriverCar {
 
-    public void onCallBackDriverCarCategory(String carcategory);
+    public void onCallBackDriverCarCategory(String driversCarCategory);
 }
