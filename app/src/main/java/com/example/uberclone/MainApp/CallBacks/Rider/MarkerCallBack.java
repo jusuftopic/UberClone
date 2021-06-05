d9df18e28f072d3f0f8672c78933552be117b1c2
Jusuf <jusuftopic@yahoo.com>
2021-06-05 06:41:23 +0200

    created marker callback

---
+++ app/src/main/java/com/example/uberclone/MainApp/CallBacks/Rider/MarkerCallBack.java
@@ -0,0 +1,8 @@
+package com.example.uberclone.MainApp.CallBacks.Rider;
+
+import com.google.android.gms.maps.model.Marker;
+
+public interface MarkerCallBack {
+
+    public void onAddedMarker(Marker marker);
+}
