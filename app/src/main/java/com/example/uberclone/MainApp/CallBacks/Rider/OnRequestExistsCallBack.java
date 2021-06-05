b085429ec8de1c02f138e4e7a65c5b0558d3f5a3
Jusuf <jusuftopic@yahoo.com>
2021-06-05 07:37:31 +0200

    callbacks for driver loc

---
+++ app/src/main/java/com/example/uberclone/MainApp/CallBacks/Rider/OnRequestExistsCallBack.java
@@ -0,0 +1,6 @@
+package com.example.uberclone.MainApp.CallBacks.Rider;
+
+public interface OnRequestExistsCallBack {
+
+    public void onRequestExistsCallback(boolean exists);
+}
---
+++ app/src/main/java/com/example/uberclone/MainApp/CallBacks/Rider/OnRequestLatLang.java
@@ -0,0 +1,8 @@
+package com.example.uberclone.MainApp.CallBacks.Rider;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public interface OnRequestLatLang {
+
+    public void onRequestsLatLang(LatLng latLng);
+}
