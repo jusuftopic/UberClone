198852a4f80adb64e54dcebd34ba81006917f3db
Jusuf <jusuftopic@yahoo.com>
2021-06-07 06:35:30 +0200

    callback on DriverLatLng

---
+++ app/src/main/java/com/example/uberclone/MainApp/CallBacks/AcceptedRequests/DriverLatLngCallBack.java
@@ -0,0 +1,8 @@
+package com.example.uberclone.MainApp.CallBacks.AcceptedRequests;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public interface DriverLatLngCallBack {
+
+    public void onDriverLatLng(LatLng driverLatLng);
+}
